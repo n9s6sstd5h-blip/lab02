48bef8bff6e0f616622295c55e54f3b629477eef
n9s6sstd5h-blip <n9s6sstd5h@privaterelay.appleid.com>
2026-06-10 00:02:19 +0300

    Add comments to hello world

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,10 @@
 
 int main()
 {
+    // Store the user name read from standard input.
     std::string name;
+
+    // Ask for a name and print a personalized greeting.
     std::cout << "Enter your name: ";
     std::cin >> name;
     std::cout << "Hello world from " << name << std::endl;
